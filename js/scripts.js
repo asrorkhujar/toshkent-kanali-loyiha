1a42dfb77c5a7eb4d56bf74c1a7bffb40f556df5
coder-1990 <asrorakmalovich@gmail.com>
2022-02-11 23:50:07 +0500

    add loader

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
-var elLightThemeButton = document.querySelector('.js-light-theme-button');
-var elDarkThemeButton = document.querySelector('.js-dark-theme-button');
+let elLightThemeButton = document.querySelector('.js-light-theme-button');
+let elDarkThemeButton = document.querySelector('.js-dark-theme-button');
 elLightThemeButton.addEventListener('click', function(){
     document.body.classList.remove('dark-mode');
 })
 elDarkThemeButton.addEventListener('click',
 function(){
     document.body.classList.add('dark-mode');
-})
+});
+
+let elMask = document.querySelector(".mask");
+
+window.addEventListener("load", () => {
+    elMask.classList.add("hide");
+    setTimeout(() => {
+        elMask.remove();
+    }, 600);
+});
